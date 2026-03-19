f8283e7f4e8e190864ca18aa605467c70e2f81ec
Mina Adel Zaki Gaballa <mina.adel.zaki.gaballa@gmail.com>
2026-03-20 00:21:09 +0200

    Clean up repository and add final documentation structure

--- imports.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_container_registry.acr
-  id = "/subscriptions/d6680869-f124-4533-9a52-b6b23e560f3c/resourceGroups/aks-terraform-arc-rg/providers/Microsoft.ContainerRegistry/registries/minaaksacr20260319"
-}
